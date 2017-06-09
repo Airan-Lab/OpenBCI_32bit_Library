f5395ee46551b4ade88c604cbbd7ab39041ce622
Jeffrey Wang <Jeffrey.Bond.Wang@gmail.com>
2017-06-09 11:31:59 -0500

    Prepping for upload to lab comp

--- EEG_Board/OBCI_stim_FUS_triggered/OBCI_stim_FUS_triggered.ino
+++ EEG_Board/OBCI_stim_FUS_triggered/OBCI_stim_FUS_triggered.ino
@@ -1,4 +1,4 @@
-// Main Loop for OBCI system to be triggered from the 
+// Main Loop for OBCI system to be triggered from the
 // Adapted by Jeffrey Wang (@dancingdarwin) from pushbutton Contributed example by Irene Vigue Guix (@viguix)
 
 #include <DSPI.h>
@@ -12,23 +12,22 @@ boolean addAccelToSD = false; // On writeDataToSDcard() call adds Accel data to 
 boolean addAuxToSD = true; // On writeDataToSDCard() call adds Aux data to SD card write
 boolean SDfileOpen = false; // Set true by SD_Card_Stuff.ino on successful file open
 
-const uint8_t Rate_Adj = ADS_RATE_500Hz;
+const uint8_t Rate_Adj = ADS_RATE_1kHz;
 
 // Three External Trigger Code
 // --------------------------------------------------------
 //  << EXTERNAL TRIGGER FROM FUS and STIMULI >>
-const int fusTrig = 13;          // FUS Trigger (from IGT System)
-const int vepTrig = 17;          //  LED Stimulus Trigger (from RPi)
-const int ssepTrig = 18;         // Stimulus Isolator Trigger (from RPi)
+//const int fusTrig = 13;          // FUS Trigger (from IGT System)
+const int stimTrig = 17;          //  LED Stimulus Trigger (from RPi)
+const int fusTrig = 18;         // Stimulus Isolator Trigger (from RPi)
 int fusValue;          // used to hold the latest button reading
-int vepValue;
-int ssepValue;
+int stimValue;
 
 // --------------------------------------------------------
 
 void setup() {
   // Bring up the OpenBCI Board with the sample rate
-  /* Possible Rates: 
+  /* Possible Rates:
    * 250 Hz: ADS_RATE_250Hz
    * 500 Hz: ADS_RATE_500Hz
    * 1 kHz:  ADS_RATE_1kHz
@@ -37,7 +36,7 @@ void setup() {
    * 8 kHz:  ADS_RATE_8kHz
    * 16 kHz: ADS_RATE_16kHz
    */
-  board.beginDebug(ADS_RATE_500Hz);
+  board.beginDebug(ADS_RATE_1kHz);
 
   // Notify the board we don't want to use accel data
   board.useAccel = false;
@@ -46,49 +45,43 @@ void setup() {
   board.useAux = true;
 
   // Configure two external triggers
-  pinMode(fusTrig, INPUT);    // set the button pin direction
-  pinMode(vepTrig, INPUT);
-  pinMode(ssepTrig, INPUT);
+  pinMode(stimTrig, INPUT);    // set the button pin direction
+  pinMode(fusTrig, INPUT);
+  //digitalWrite(fusTrig, LOW);
+  //pinMode(ssepTrig, INPUT);
 }
 
 void loop() {
   if (board.streaming) {
+    // Read from Stim
+    stimValue = digitalRead(stimTrig);
+    if (stimValue == HIGH) {  // if it's active
+      board.auxData[0] = board.auxData[0] | 0x0001;
+      addAuxToSD = true;             // add Aux Data to the SD card if it's there
+    }
+
+    // Read from FUS
+    fusValue = digitalRead(fusTrig);
+    if (fusValue == HIGH) {  // if it's active
+      board.auxData[0] = board.auxData[0] | 0x0002;
+      addAuxToSD = true;             // add Aux Data to the SD card if it's there
+    }
+    
     if (board.channelDataAvailable) {
       // Read from the ADS(s), store data, set channelDataAvailable flag to false
       board.updateChannelData();
 
-      //Reset triggers
-      board.auxData[0] = 0x0000;
-
-      // Read from FUS
-      fusValue = digitalRead(fusTrig);
-      if (fusValue == HIGH) {  // if it's active
-        board.auxData[0] += 0x0001;
-        addAuxToSD = true;             // add Aux Data to the SD card if it's there
-      }
-
-      // Read from VEP
-      vepValue = digitalRead(vepTrig);
-      if (vepValue == HIGH) {  // if it's active
-        board.auxData[0] += 0x0002;
-        addAuxToSD = true;             // add Aux Data to the SD card if it's there
-      }
-
-      // Read from SSEP
-      ssepValue = digitalRead(ssepTrig);
-      if (ssepValue == HIGH) {  // if it's active
-        board.auxData[0] += 0x0004;
-        addAuxToSD = true;             // add Aux Data to the SD card if it's there
-      }      
-
       // Verify the SD file is open
-      if(SDfileOpen) {
+      if (SDfileOpen) {
         // Write to the SD card, writes aux data
         writeDataToSDcard(board.sampleCounter);
       }
 
       // Send standard packet with channel data, trigger data, and time stamp
       board.sendChannelDataWithTimeAndRawAux();
+
+      //Reset triggers
+      board.auxData[0] = 0x0000;
     }
   }
 
@@ -103,7 +96,7 @@ void loop() {
     // Send to the board library
     board.processChar(newChar);
   }
-  
+
   if (board.hasDataSerial1()) {
     // Read one char from the serial port
     char newChar = board.getCharSerial1();
